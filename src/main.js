2efdefd3e223fc6cd98df4a05ec83373b61227b8
Micah N Gorrell <micahg@netmail.com>
2016-10-28 17:20:00 -0600

    Correct the square the character is being drawn on when moving down the screen

--- src/main.js
+++ src/main.js
@@ -23,14 +23,19 @@ function render(ctx)
 	/* We need the same seed for every frame so the same tiles are used */
 	WRand.setSeed(12345);
 
-	// TODO Decide how many rows/columns to render
-
+	/* Determine which vertical row each character is on for this frame */
+	for (var c = 0, character; character = world.characters[c]; c++) {
+		if (character.animation && character.animation.dy > 0) {
+			character.ry = 1;
+		} else {
+			character.ry = 0;
+		}
+	}
 
 	/*
 		Render each row from the top down, so a row closer to the player can
 		draw over the row behind it.
 	*/
-
 	for (var y = 0, row; (row = world.rows[y]) && y < world.viewport.height; y++) {
 		for (var x = 0, tile; (tile = row.charAt(x)) && 1 == tile.length && x < world.viewport.width; x++) {
 			if (tile === ' ') {
@@ -64,17 +69,11 @@ function render(ctx)
 		}
 
 		for (var c = 0, character; character = world.characters[c]; c++) {
-			// TODO Is this character on this row? If the character is moving
-			//		then their position may be between rows now and shouldn't
-			//		be drawn yet if a portion of the character is on the lower
-			//		row.
-
-			// TODO Draw the character
 			if (!character.images) {
 				character.images = {};
 			}
 
-			if (character.y !== y) {
+			if (character.y + character.ry !== y) {
 				continue;
 			}
 
