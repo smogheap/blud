0d54b863f68336a099968e166589a2a5e2d9b4e1
Micah N Gorrell <micahg@netmail.com>
2017-01-16 22:32:58 -0700

    Allow starting as an electron app

---
+++ app.js
@@ -0,0 +1,35 @@
+const electron		= require('electron');
+const app			= electron.app;
+const BrowserWindow	= electron.BrowserWindow;
+
+/*
+	Keep a global reference to the window object to keep it from being garbage
+	collected which will close it.
+*/
+let mainWindow;
+
+function createWindow()
+{
+	mainWindow = new BrowserWindow({ width: 800, height: 600, frame: false });
+	mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+	// mainWindow.webContents.openDevTools();
+
+	mainWindow.on('closed', function () {
+		mainWindow = null;
+	});
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', function ()
+{
+	app.quit();
+});
+
+app.on('activate', function ()
+{
+	if (!mainWindow) {
+		createWindow();
+	}
+});
+
